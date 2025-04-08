5d6deb3489b539864ad47d4653cafef1e63b5d8e
NigarHasanzada <nigarnh-ap216@code.edu.az>
2025-04-08 11:06:51 +0400

    Task14 functions

---
+++ 14.Functions/assets/js/mathfunction.js
@@ -0,0 +1,13 @@
+
+
+function MathFunc(a,b) {
+    return {
+        sum: a + b,
+        difference: a - b,
+        product: a * b,
+        quotient: a / b
+    }
+
+    
+}
+console.log(MathFunc(6,7));
